cb5020cc2031af79b1e73dab90f469daec43a870
unknown <datoro@unal.edu.co>
2023-08-08 20:16:55 -0500

    Id autogeneration using timestamp

--- accounts/admin.py
+++ accounts/admin.py
@@ -13,7 +13,7 @@ class MinervaUserAdmin(UserAdmin):
     add_form = MinervaUserCreationForm
     form = MinervaUserChangeForm
     model = User
-    list_display = ("email", "first_name", "last_name", "is_staff", "is_active",)
+    list_display = ("id" ,"email", "first_name", "last_name", "is_staff", "is_active",)
     list_filter = ("email", "first_name", "last_name", "is_staff", "is_active",)
     fieldsets = (
         (None, {"fields": ("email", "password", "first_name", "last_name")}),
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .managers import MinervaUserManager
+from datetime import datetime
 
 # Create your models here.
 # TODO: cambiar el uso de unique username a unique email
@@ -15,9 +16,18 @@ class User(AbstractUser):
     username=None
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
-
-    
+    id=models.BigIntegerField(primary_key=True, editable=False)
+    # class Meta:
+    #     db_table = 'user'
+    #     # Indica el valor inicial y el incremento para el campo AutoField
+    #     # El valor de 'start_with' establece el valor inicial, y 'increment_by' establece el incremento
+    #     options = {'starting_value': 1000, 'increment_by': 1}
 
     objects = MinervaUserManager()
     def __str__(self):
         return(self.get_full_name())
+    
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = int(datetime.now().timestamp() * 1000)
+        super().save(*args, **kwargs)
