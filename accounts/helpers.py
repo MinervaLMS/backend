368132929507c10c30cfcd46eef0afe3b4def0dc
SSR-Med <sasalazarr@unal.edu.co>
2023-08-10 21:02:53 -0500

    Settings and password link fixed

--- accounts/helpers.py
+++ accounts/helpers.py
@@ -18,7 +18,7 @@ def send_forgot_email(email: str, token: str, uidb64: str) -> bool:
     """
 
     subject = 'Link to recover password'
-    message = f'Hi, click on the link to reset your password http://127.0.0.1:8000/change-password/{uidb64}/{token}/'
+    message = f'Hi, click on the link to reset your password http://127.0.0.1:8000/password-reset/{uidb64}/{token}'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, email_from, recipient_list)
