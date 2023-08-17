257d9188613ca117ae50b64ef414065aa18f6f86
eporrasm <vals4oo@gmail.com>
2023-08-17 18:11:05 -0500

    CRUD Curso Funcional
    Corregido error en GET

--- courses/views.py
+++ courses/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, permission_classes, schema
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from django.shortcuts import render
 from .serializers import CourseSerializer
@@ -9,6 +10,7 @@ from .models import Course
 from . import schemas
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 @schema(schemas.create_course_schema)
 def create_course(request) -> JsonResponse:
     """
@@ -28,6 +30,7 @@ def create_course(request) -> JsonResponse:
     return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 @schema(schemas.get_course_schema)
 def get_course(request, alias: str) -> JsonResponse:
     """
@@ -47,11 +50,10 @@ def get_course(request, alias: str) -> JsonResponse:
         return JsonResponse({"error": "Course not found"}, status=status.HTTP_404_NOT_FOUND)
 
     serializer = CourseSerializer(course)
-    if serializer.is_valid():
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
-    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+    
 @api_view(['PATCH'])
+@permission_classes([IsAuthenticated])
 @schema(schemas.update_course_schema)
 def update_course(request, alias: str) -> JsonResponse:
     """
@@ -77,6 +79,7 @@ def update_course(request, alias: str) -> JsonResponse:
     return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
 @schema(schemas.delete_course_schema)
 def delete_course(request, alias: str) -> JsonResponse:
     """
