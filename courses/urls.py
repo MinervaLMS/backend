07334fcd8e2569e950fba7817fd94e9e234d04c1
eporrasm <vals4oo@gmail.com>
2023-08-18 18:48:14 -0500

    Correccion url course/<alias>/
    Estaba course/get/<alias>/

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 course_urls = [
     path('course/create/', views.create_course, name='create_course'),
-    path('course/get/<str:alias>/', views.get_course, name='get_course'),
+    path('course/<str:alias>/', views.get_course, name='get_course'),
     path('course/update/<str:alias>/', views.update_course, name='update_course'),
     path('course/delete/<str:alias>/', views.delete_course, name='delete_course'),
 ]
