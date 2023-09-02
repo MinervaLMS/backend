7c5a1270592cb22c110e2b22204a462394ff474f
eporrasm <vals4oo@gmail.com>
2023-09-01 21:26:07 -0500

    agregados nuevos atributos a course

--- courses/models/course.py
+++ courses/models/course.py
@@ -6,7 +6,15 @@ class Course(models.Model):
     name = models.CharField(max_length=100, blank=False, unique=True)
     alias = models.CharField(max_length=20, blank=False, unique=True)
     description = models.TextField(blank=True)
+    course_instructional_materials = models.IntegerField(default=0)
+    course_assessment_materials = models.IntegerField(default=0)
+    course_extra_materials = models.IntegerField(default=0)
+    min_assessment_progress = models.IntegerField(default=70)
+    average_stars = models.IntegerField(null=True, blank=True)
+    appraisals = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
 
+    parent_course = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     enrollments = models.ManyToManyField("accounts.User", through='Enrollment')
 
     def __str__(self):
