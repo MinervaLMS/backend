578f07297cc1d44370f0d0f4dc76088b2fd3b0ff
dmolinat <dmolinat@unal.edu.co>
2023-09-04 22:49:40 -0500

    Update material_io_code_tests.py

---
+++ ioc/tests/material_io_code_tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from courses.models.material import Material
+from ..models.material_io_code import MaterialIoCode
+
+
