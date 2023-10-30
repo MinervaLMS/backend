f3f63829d4690d007f246915b7dfedff2b0ae2fd
EsGarciaC <esgarciac00@gmail.com>
2023-10-30 11:38:51 -0500

    Made PDF material migrations

--- courses/models/__init__.py
+++ courses/models/__init__.py
@@ -3,6 +3,7 @@ from .module import Module
 from .material import Material
 from .material_html import MaterialHTML
 from .material_video import MaterialVideo
+from .material_pdf import MaterialPDF
 from .enrollment import Enrollment
 from .access import Access
 from .instructor import Instructor
@@ -13,6 +14,7 @@ _ = [
     Material,
     MaterialHTML,
     MaterialVideo,
+    MaterialPDF,
     Enrollment,
     Access,
     Instructor,
