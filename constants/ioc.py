dbf6b04f152f8722056721da846ba830f87df5ac
Cristian Camilo Henao Rojas <41649200+Tholes@users.noreply.github.com>
2023-11-03 09:16:02 -0500

    Update ioc.py

--- constants/ioc.py
+++ constants/ioc.py
@@ -1,5 +1,5 @@
 """ioc constants"""
 
-URL_JUDGE = "http://3.16.113.19:5000/judge/end"
-URL_PROBLEM = "http://3.16.113.19:5000/problem"
+URL_JUDGE = "http://3.21.114.121:5000/judge/end"
+URL_PROBLEM = "http://3.21.114.121:5000/problem"
 LIST_IOC = ["input", "output", "points", "max_time", "max_memory"]
