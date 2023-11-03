c047bf21165792107b2272a8ed1dc3e3cd22d020
Emmanuel López Rodríguez <contact@emlopezr.com>
2023-11-02 20:29:47 -0500

    Change Judge URL

--- constants/ioc.py
+++ constants/ioc.py
@@ -1,5 +1,5 @@
 """ioc constants"""
 
-URL_JUDGE = "http://3.134.96.35:5000/judge/end"
-URL_PROBLEM = "http://3.134.96.35:5000/problem"
+URL_JUDGE = "http://3.16.113.19:5000/judge/end"
+URL_PROBLEM = "http://3.16.113.19:5000/problem"
 LIST_IOC = ["input", "output", "points", "max_time", "max_memory"]
